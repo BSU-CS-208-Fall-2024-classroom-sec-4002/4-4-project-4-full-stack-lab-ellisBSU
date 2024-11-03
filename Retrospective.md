eaf107f31d2238bb69664987a2a971205cd0b492
Ellis <ellisrodriguez@u.boisestate.edu>
2024-11-03 16:33:29 -0700

    Project 4 is complete.

--- Retrospective.md
+++ Retrospective.md
@@ -1,16 +1,20 @@
 # Retrospective
 
-- name: Jane Doe
-- email: janedoe@u.boisestate.edu
+- name: Ellis Rodriguez
+- email: ellisrodrigueza@u.boisestate.edu
 
 ## Experience
 
-TODO
+I tested my project by starting the server and manually doing get, post, and delete requests. I did this to make sure the behavior was working as intended. I struggled with figuring out why the server could not read the local variable 'local' on a get request but after some time I realized that I was only passing the 'local' variable inside the post request in the 'render' function and not in the get request too. Nothing was poorly specified. I knew how the web page had to interact for it to be considered complete because of the walkthrough video that was included. I was able to get the entire project done by following the examples that were included in the assignment. One new thing I learned from this project was how to start using a database inside of a server. The database allows developers to do all the CRUD operations through the functionality that is implemented in the database module. One thing I would like to share is that I had a fun time learning how to build a simple full stack project. I like how the walkthrough video noted that this project was similar to a "Hello World" program where it was mostly focused on building familiarity with full stack development therefore there was nothing too tricky about the project. I also liked how data in a database could be stored in memory instead of some file which makes it easier for testing since I am not interested in that data after I have finished testing. 
 
 ## Known issues or Bugs
 
-TODO
+NONE
 
 ## Sources used
 
-TODO
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit - I used this source to figure out how to change the text value of
+the submit button. 
+
+https://www.w3schools.com/tags/tag_hr.asp - I used this source to figure out how to add a horizontal line across the web page like the
+example shown. It is used to separate the input textbox and the todo list. According to this source, it is done by adding the hr tag. 
--- src/server.js
+++ src/server.js
@@ -18,27 +18,95 @@ app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
+/**
+ * A get request that displays a webpage with an input box for a new task from the user along with the todo list 
+ * of all the tasks that the user still has to do. 
+ * 
+ * @param {String} The route of the get request.  
+ * @param {Callback Function} A function that handles the request and response for the get request.
+ */
 app.get('/', function (req, res) {
-    //TODO You will need to do a SQL select here
-    //TODO You will need to update the code below!
+    const local = { tasks: [] }
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+            console.log(err)
+        } else {
+            local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, numrows) {
+        if (!err) {
+            res.render('index', local)
+        } else {
+            console.log(err)
+        }
+    })
     console.log('GET called')
-    res.render('index')
-
 })
 
+/**
+ * Posts the user-provided task to the database in the server and then displays
+ * the new task in the todo list. 
+ * 
+ * @param {String} The route of the post request.
+ * @param {Callback Function} A function that handles the request and response for the post request.
+ */
 app.post('/', function (req, res) {
     console.log('adding todo item')
-    //TODO You will need to to do a SQL Insert here
-
+    const stmt = db.prepare('INSERT INTO todo (task) VALUES (?)')
+    stmt.run(req.body.todo)
+    stmt.finalize()
+    const local = { tasks: [] }
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+            console.log(err)
+        } else {
+            local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, numrows) {
+        if (!err) {
+            res.render('index', local)
+        } else {
+            console.log(err)
+        }
+    })
 })
 
+/**
+ * A post request that deletes the task a user specified and then removes the task from the todo list
+ * displayed in the web page. 
+ * 
+ * @param {String} The route of the post request.  
+ * @param {Callback Function} A function that handles the request and response for the post request.
+ */
 app.post('/delete', function (req, res) {
     console.log('deleting todo item')
-    //TODO you will need to delete here
-
+    const stmt = db.prepare('DELETE FROM todo where id = (?)')
+    stmt.run(req.body.id)
+    stmt.finalize()
+    const local = { tasks:[] }
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+            console.log(err)
+        } else {
+            local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, numrows) {
+        if (!err) {
+            res.render('index', local)
+        } else {
+            console.log(err)
+        }
+    })
 })
 
 // Start the web server
+/**
+ * Begins listening for requests on the port specified in the function's parameter.
+ * 
+ * @param {Number} The port number that the server should be getting requests on. 
+ * @param {Callback Function} A callback function that logs the port that the server is listening
+ * on to the console. 
+ */
 app.listen(3000, function () {
     console.log('Listening on port 3000...')
 })
